8d031a15f2808e633e7a2ec70d187db6956091e1
Andreas Heumaier <andreas.heumaier@microsoft.com>
2021-01-18 18:01:31 +0100

    fix(armkit): delete props.armResourceType

--- packages/@armkit/core/lib/arm-resource.ts
+++ packages/@armkit/core/lib/arm-resource.ts
@@ -14,6 +14,7 @@ export abstract class ArmResource extends ArmElement {
     super(scope, id)
 
     this.armResourceType = props.armResourceType;
+    delete props.armResourceType
     this._armProperties = props || {};
   }
 
