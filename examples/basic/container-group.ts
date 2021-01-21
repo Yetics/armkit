1652bccf6a4f47fbfa9cc3152ebcdffa42c0adb5
Andreas Heumaier <andreas.heumaier@microsoft.com>
2021-01-21 22:36:58 +0100

    docs: Updated, extended  examples

---
+++ examples/basic/container-group.ts
@@ -0,0 +1,43 @@
+import { Construct } from 'constructs';
+import { App, ArmStack } from '@armkit/core';
+import {
+    ContainerGroups, ContainerGroupPropertiesOsTypeEnum, MicrosoftContainerInstanceContainerGroupsTypeEnum, MicrosoftContainerInstanceContainerGroupsApiVersionEnum
+} from './.generated/ContainerInstance'
+
+export class HelloContainerGroup extends ArmStack {
+    constructor(scope: Construct, id: string) {
+        super(scope, id);
+
+        new ContainerGroups(this, 'MyContainerGroup', {
+            name: 'azurecdktest',
+            location: 'westeurope',
+            apiVersion: MicrosoftContainerInstanceContainerGroupsApiVersionEnum['2019_12_01'],
+            type: MicrosoftContainerInstanceContainerGroupsTypeEnum.MICROSOFT_CONTAINER_INSTANCE_CONTAINER_GROUPS,
+            properties: {
+                containers: [{
+                    name: 'ubuntu-server',
+                    properties: {
+                        image: 'ubuntu:18.04',
+                        command: ['sleep infinity'],
+                        resources: {
+                            requests: {
+                                cpu: 1,
+                                memoryInGb: 2
+                            },
+                            limits: {
+                                cpu: 1,
+                                memoryInGb: 2
+                            }
+                        }
+
+                    }
+                }],
+                osType: ContainerGroupPropertiesOsTypeEnum.LINUX,
+            }
+        })
+    }
+}
+
+const app = new App({ outdir: 'cdk.out' });
+new HelloContainerGroup(app, 'hello-container-group');
+app.synth();
